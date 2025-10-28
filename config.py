3a1e82a36ea693c2255f55e60fa675df293728b0
kapiljaat320 <kapijaat320@gmail.com>
2025-10-28 09:00:33 +0530

    Update environment variables in config.py

--- config.py
+++ config.py
@@ -7,20 +7,20 @@ API_ID = int(os.environ.get("API_ID", "28021723"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH","f6b0736c56e8544e6d55b7c4ef169a68")
 # ------------------------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8443165953:AAGi-I1_V2L_UAxuGqY02mD35VaOoNVFPLU")
 # ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "mahi_extratorBot")
 BOT_TEXT = ":𝐈𝐓'𝐬𝐆𝐎𝐋𝐔.™®:"
 # ------------------------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6312174113"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003226549671"))
 
 # //FORCE_CHANNEL_ID
 CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "")) 
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://kapijaat123:diyT9qB58dROlYQI@cluster0.ofxrqxt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
 PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", ""))
 # -----------------------------------------------
